a5bd979f34ca917634dffd1c0822d67f07bcdcb3
Josh Holloway <j@holloway.me>
2011-08-27 14:27:03 -0500

    first tree life cycle idea (rigid)

--- orangeTree.rb
+++ orangeTree.rb
@@ -1,10 +1,46 @@
 class OrangeTree
   
   def initialize
+    
     @treeAge = 0
+    
     @treeHeight = 0
-    @mature = false
+    
+    @treeMaturity = 0
+    
+    # Maturity 0 - For the first three years, the tree will not bear fruit.
+    #               The tree will grow two feet every year.
+    
+    #               @treeAge = 3
+    
+    # Maturity 1 - After the third year, the tree will bear bitter fruit.
+    #               The tree will grow one foot every year.
+    #               The tree will bear 25 oranges per year.
+    
+    #               @treeAge = 7
+    
+    # Maturity 2 - After the seventh year, the tree will bear good fruit.
+    #               The tree will grow .75 feet every year.
+    #               The tree will bear 100 oranges per year.
+    
+    #               @treeAge = 28
+    
+    # Maturity 3 - After the 28th year, the tree will continue to bear good fruit.
+    #               The tree will cease growing.
+    #               The tree will bear 200 oranges per year.
+    
+    #               @treeAge = 50
+    
+    # Maturity 4 - After the 50th year, the tree will continue to bear good fruit.
+    #               The tree will not grow.
+    #               The tree will bear 300 oranges per year.
+    
+    #               @treeAge = 100
+    
+    # Death      - After the 100th year, the tree will die.
+    
     puts 'You planted an orange tree.'
+    
   end
   
   def getHeight
@@ -12,7 +48,7 @@ class OrangeTree
     puts "The tree is #{@treeHeight} feet tall."
   end
   
-  def one_year_passes
+  def oneYearPasses
     @treeAge = @treeAge + 1
   end
   
@@ -20,5 +56,5 @@ end
 
 tree = OrangeTree.new
 tree.getHeight
-tree.one_year_passes
+tree.oneYearPasses
 tree.getHeight

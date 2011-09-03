5a49e36be6eec903419e4e342863cb3f6fbdc9e0
Josh Holloway <j@holloway.me>
2011-09-02 22:05:12 -0500

    Started "Learn Ruby the Hard Way"

---
+++ ex3.rb
@@ -0,0 +1,34 @@
+# Print the first line.
+puts "I will now count my chickens:"
+
+#Print a line, do some additon and division
+puts "Hens", 25.0 + 30.0 / 6.0
+# Print a line, do some subtraction, multiplication, and modulus operations
+puts "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+# Print another line
+puts "Now I will count the eggs:"
+
+# Do a bunch of math
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# Print another line
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Do a comparison
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# Print more lines and do more math
+puts "What is 3 + 2?", 3.0 + 2.0
+puts "What is 5 - 7?", 5.0 - 7.0
+
+# Print another line
+puts "Oh, that's why it's false."
+
+# ...and another
+puts "How about some more."
+
+# Do three more comparisons
+puts "Is it greater?", 5.0 > -2.0
+puts "Is it greater or equal?", 5.0 >= -2.0
+puts "Is it less or equal?", 5.0 <= -2.0
---
+++ ex5_2.rb
@@ -0,0 +1,3 @@
+float = 24.5
+
+puts "The number #{float} is a float."
